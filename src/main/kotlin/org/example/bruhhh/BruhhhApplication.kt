6c393274ff68574857af5592dc849c9336dd9baf
ffrntthng <firuzamammadli04@gmail.com>
2024-10-25 14:09:36 +0400

    aahhahaha

---
+++ src/main/kotlin/org/example/bruhhh/BruhhhApplication.kt
@@ -0,0 +1,11 @@
+package org.example.bruhhh
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BruhhhApplication
+
+fun main(args: Array<String>) {
+    runApplication<BruhhhApplication>(*args)
+}
